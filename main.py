8519696ba47d4e66695112e55337c99ebc2bba99
NumPix <lenchik221006@gmail.com>
2023-06-14 08:42:08 +0300

    started adding actual gameplay lol

--- main.py
+++ main.py
@@ -1,11 +1,63 @@
 import random
 
+from Objects.color import color
 from Battle import *
 from Data.attacks import *
 from Data.items import *
 
 
+def new_game():
+    print(color.BOLD + "Chose a starter pokemon:" + color.END)
+
+    list_of_starters = [
+        "Bulbasaur",
+        "Charmander",
+        "Squirtle",
+        "Chikorita",
+        "Cindaquil",
+        "Totodile",
+        "Treecko",
+        "Torchik",
+        "Mudkip",
+        "Turtwig",
+        "Chimchar",
+        "Piplup",
+        "Snivy",
+        "Tepig",
+        "Oshawott",
+        "Chespin",
+        "Fennekin",
+        "Froakie",
+    ]
+
+    print(*[f"({poke[0]}) {poke[1]}" for poke in enumerate(list_of_starters)], sep="\n", end="\n\n")
+
+    ch_poke = input().strip()
+
+    while ch_poke not in range(18):
+        print(*[f"({poke[0]}) {poke[1]}" for poke in enumerate(list_of_starters)], sep="\n", end="\n\n")
+        ch_poke = input().strip()
+
+    print(color.BOLD + "Good! Let your own adventure begin!" + color.END + "\n")
+
+    return ch_poke
+
+
 def main():
+    print(color.BOLD + "Welcome to Pokemon Battles!\n" + color.END)
+    print(*["(0) •New Game", "(1) •Continue", "(2) •Exit"], sep="\n", end="\n\n")
+
+    menu_option = input().strip()
+
+    while menu_option not in ["0", "1", "2"]:
+        print(*["(0) •New Game", "(1) •Continue", "(2) •Exit"], sep="\n", end="\n\n")
+        menu_option = input().strip()
+
+    if menu_option == "0":
+        starter_poke = new_game()
+
+
+"""def main():
     with open("config.json") as cnf:
         config = json.load(cnf)
 
@@ -33,7 +85,7 @@ def main():
         FoesTeam[i].expGain(random.randint(200000, 300000), False)
 
     battle(YourTeam, FoesTeam, "Izuna", "Glacie", Bag)
-
+"""
 
 if __name__ == '__main__':
     main()
